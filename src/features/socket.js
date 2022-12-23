23974551f6e3c10ad7915c772d293465df2c6a41
giguchi <hil.giga@gmail.com>
2022-12-23 15:10:18 +0000

    try provide socket server runnin on local

--- src/features/socket.js
+++ src/features/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client"
 
-const socket = io("http://localhost:46421", {autoConnect: false})
+const socket = io("http://localhost:8900", {autoConnect: false})
 
 
 export default socket
