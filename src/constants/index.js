f66410a26ee2860c2e0b6dc3223c26956859de9b
giguchi <hil.giga@gmail.com>
2023-01-10 11:23:15 +0000

    alter socket server url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL = "https://api-vnuws.ondigitalocean.app"
-export const SOCKET_SERVER = "http://localhost:8900"
+export const SOCKET_SERVER = "https://socket-server-znhxw.ondigitalocean.app/"
