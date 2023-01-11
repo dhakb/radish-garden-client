cd2bafd3b9fc3d2d385917dd8cc19e14c64ca35a
giguchi <hil.giga@gmail.com>
2023-01-11 11:16:39 +0000

    configure #costum start scripts

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL = "https://api-vnuws.ondigitalocean.app"
-export const SOCKET_SERVER = "https://socket-server-znhxw.ondigitalocean.app/"
+// export const SOCKET_SERVER = "https://socket-server-znhxw.ondigitalocean.app/"
+export const SOCKET_SERVER = "http://localhost:8900/"
